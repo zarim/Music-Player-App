a2a0c7c383897cf2c0691cd6126b4f3eb745ab90
Zari McFadden <zarimcfadden@WTA-MBP427.attlocal.net>
2021-07-14 16:27:55 -0400

    started project

---
+++ Shared/Music_Player_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_Player_AppApp.swift
+//  Shared
+//
+//  Created by Zari McFadden on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Music_Player_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
